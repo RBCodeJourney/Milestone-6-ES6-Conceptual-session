e22ef9e72beaf630d185c963f1e599c905914c31
RBCodeJourney <refatullahbiswas@gmail.com>
2023-11-21 01:53:32 +0530

    about arrow function, arrow.js file added

---
+++ arrow.js
@@ -0,0 +1,36 @@
+// function add(x, y) {
+//     const result = x + y;
+//     return result;
+// }
+
+// const add = (x, y) => {
+//     const result = x + y;
+//     return result;
+// }
+
+
+
+
+// function show() {
+//     console.log('hello World')
+// }
+// function printNumber(x) {
+//     console.log("value is", x)
+// }
+
+
+
+// console.log(add(2, 3));
+// show()
+// printNumber(49)
+
+const add = (x, y) => x + y;
+
+const show = () => {
+    console.log('hello')
+}
+show()
+console.log(add(4, 5))
+
+const showNumber = x => console.log(x);
+showNumber(40);
