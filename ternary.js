bdd069adab1a23a3f4550962b608004ec5ff00f7
RBCodeJourney <refatullahbiswas@gmail.com>
2023-11-22 18:27:15 +0530

    ternary.js file added

---
+++ ternary.js
@@ -0,0 +1,17 @@
+const x = 3;
+
+/* 
+if (x > 5) {
+    console.log('x is greater than 5')
+} else {
+    console.log('x is less than 5')
+}
+  */
+ 
+//  অথবা এভাবেও করা যায় 
+
+x > 5
+    ?
+    console.log('x is greater than 5')
+    :
+    console.log('x is less than 5');
