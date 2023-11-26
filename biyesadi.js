48997837abd88b2c895577670109158b06169bd3
RBCodeJourney <refatullahbiswas@gmail.com>
2023-11-26 11:37:26 +0530

    biyesadi.js file added

---
+++ biyesadi.js
@@ -0,0 +1,18 @@
+const polapain = [
+    { name: "Abul", job: "sorkari", salary: 17000 },
+    { name: "Babul", job: "besorkari", salary: 25000 },
+    { name: "Kabul", job: "sorkari", salary: 21000 },
+    { name: "Habul", job: "besorkari", salary: 47000 },
+    { name: "Jabul", job: "sorkari", salary: 23000 },
+    { name: "Mabul", job: "besorkari", salary: 55000 },
+];
+
+const vaggobans = polapain.filter((pola) => (pola.job === 'sorkari' && pola.salary >= 20000) || (pola.job === 'besorkari' && pola.salary >= 40000));
+
+const polaNumber = Math.floor(Math.random() * vaggobans.length)
+
+const finalJamai = vaggobans[polaNumber].name;
+console.log(`${finalJamai}, Tumi jistso....!`)
+
+
+
